213b40de4e0d5e5ab2973c180ce326acaa7edc31
ufownl <ufownl@gmail.com>
2017-08-29 14:59:48 +0800

    Add xml encode interface

--- src/cppxml.cpp
+++ src/cppxml.cpp
@@ -1,8 +1,125 @@
 #include "cppxml.hpp"
 #include <rapidxml.hpp>
+#include <rapidxml_print.hpp>
 #include <vector>
+#include <iterator>
+#include <algorithm>
 
 namespace {
+
+void to_xml(lua_State* L, int index, rapidxml::xml_document<>& doc,
+            rapidxml::xml_node<>* root = nullptr) {
+  if (index <= 0 || !lua_istable(L, index)) {
+    return;
+  }
+  lua_pushstring(L, "type");
+  lua_gettable(L, index);
+  auto type = lua_tointeger(L, -1);
+  lua_pop(L, 1);
+  auto visit_children = [&](rapidxml::xml_node<>* node) {
+    lua_pushstring(L, "children");
+    lua_gettable(L, index);
+    if (lua_istable(L, -1)) {
+      lua_pushnil(L);
+      while (lua_next(L, -2)) {
+        to_xml(L, lua_gettop(L), doc, node);
+        lua_pop(L, 1);
+      }
+    }
+    lua_pop(L, 1);
+  };
+  auto visit_attributes = [&](rapidxml::xml_node<>* node) {
+    lua_pushstring(L, "attributes");
+    lua_gettable(L, index);
+    if (lua_istable(L, -1)) {
+      lua_pushnil(L);
+      while (lua_next(L, -2)) {
+        if (lua_istable(L, -1)) {
+          lua_pushstring(L, "name");
+          lua_gettable(L, -2);
+          size_t name_len = 0;
+          auto name_src = lua_tolstring(L, -1, &name_len);
+          auto name = doc.allocate_string(name_src, name_len);
+          lua_pop(L, 1);
+          lua_pushstring(L, "value");
+          lua_gettable(L, -2);
+          size_t value_len = 0;
+          auto value_src = lua_tolstring(L, -1, &value_len);
+          auto value = doc.allocate_string(value_src, value_len);
+          lua_pop(L, 1);
+          auto attr = doc.allocate_attribute(name, value, name_len, value_len);
+          node->append_attribute(attr);
+        }
+        lua_pop(L, 1);
+      }
+    }
+    lua_pop(L, 1);
+  };
+  switch (type) {
+  case rapidxml::node_document:
+    visit_children(&doc);
+    break;
+  case rapidxml::node_element:
+    if (root) {
+      lua_pushstring(L, "name");
+      lua_gettable(L, index);
+      size_t name_len = 0;
+      auto name_src = lua_tolstring(L, -1, &name_len);
+      auto name = doc.allocate_string(name_src, name_len);
+      lua_pop(L, 1);
+      auto node = doc.allocate_node(static_cast<rapidxml::node_type>(type),
+                                    name, nullptr, name_len, 0);
+      visit_attributes(node);
+      visit_children(node);
+      root->append_node(node);
+    }
+    break;
+  case rapidxml::node_data:
+  case rapidxml::node_cdata:
+  case rapidxml::node_comment:
+  case rapidxml::node_doctype:
+    if (root) {
+      lua_pushstring(L, "value");
+      lua_gettable(L, index);
+      size_t value_len = 0;
+      auto value_src = lua_tolstring(L, -1, &value_len);
+      auto value = doc.allocate_string(value_src, value_len);
+      lua_pop(L, 1);
+      auto node = doc.allocate_node(static_cast<rapidxml::node_type>(type),
+                                    nullptr, value, 0, value_len);
+      root->append_node(node);
+    }
+    break;
+  case rapidxml::node_declaration:
+    if (root) {
+      auto node = doc.allocate_node(static_cast<rapidxml::node_type>(type));
+      visit_attributes(node);
+      root->append_node(node);
+    }
+    break;
+  case rapidxml::node_pi:
+    if (root) {
+      lua_pushstring(L, "name");
+      lua_gettable(L, index);
+      size_t name_len = 0;
+      auto name_src = lua_tolstring(L, -1, &name_len);
+      auto name = doc.allocate_string(name_src, name_len);
+      lua_pop(L, 1);
+      lua_pushstring(L, "value");
+      lua_gettable(L, index);
+      size_t value_len = 0;
+      auto value_src = lua_tolstring(L, -1, &value_len);
+      auto value = doc.allocate_string(value_src, value_len);
+      lua_pop(L, 1);
+      auto node = doc.allocate_node(static_cast<rapidxml::node_type>(type),
+                                    name, value, name_len, value_len);
+      root->append_node(node);
+    }
+    break;
+  default:
+    break;
+  }
+}
 
 void to_lua(lua_State* L, rapidxml::xml_node<>* node) {
   lua_newtable(L); {
@@ -32,7 +149,9 @@ void to_lua(lua_State* L, rapidxml::xml_node<>* node) {
     default:
       break;
     }
-    if (node->type() == rapidxml::node_element) {
+    switch (node->type()) {
+    case rapidxml::node_element:
+    case rapidxml::node_declaration:
       lua_pushstring(L, "attributes");
       lua_newtable(L); {
         lua_Integer idx = 0;
@@ -50,6 +169,9 @@ void to_lua(lua_State* L, rapidxml::xml_node<>* node) {
         }
       }
       lua_settable(L, -3);
+      break;
+    default:
+      break;
     }
     switch (node->type()) {
     case rapidxml::node_document:
@@ -71,11 +193,23 @@ void to_lua(lua_State* L, rapidxml::xml_node<>* node) {
   }
 }
 
+int cppxml_encode(lua_State* L) {
+  rapidxml::xml_document<> doc;
+  to_xml(L, 1, doc);
+  std::vector<char> buf;
+  rapidxml::print(std::back_inserter(buf), doc);
+  lua_pushlstring(L, buf.data(), buf.size());
+  return 1;
+}
+
 int cppxml_decode(lua_State* L) {
   size_t len = 0;
   auto str = lua_tolstring(L, 1, &len);
   try {
-    std::vector<char> buf{str, str + len};
+    std::vector<char> buf;
+    buf.reserve(len + 1);
+    std::copy(str, str + len, std::back_inserter(buf));
+    buf.push_back(0);
     rapidxml::xml_document<> doc;
     doc.parse<0>(buf.data());
     to_lua(L, &doc);
@@ -98,6 +232,7 @@ int cppxml_decode(lua_State* L) {
 
 int luaopen_resty_cppxml(lua_State* L) {
   constexpr const luaL_Reg reg[] = {
+    {"encode", cppxml_encode},
     {"decode", cppxml_decode},
     {nullptr, nullptr}
   };
